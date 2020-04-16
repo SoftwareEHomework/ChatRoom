66656db5f886b9fdbf23d4514f535f3381dbd05c
lyh <412929473@qq.com>
2020-04-16 23:28:50 +0800

    实现聊天机器人的聊天界面

--- project/Flask_version/app.py
+++ project/Flask_version/app.py
@@ -1,4 +1,9 @@
-from flask import Flask , render_template , request, flash , redirect
+from flask import Flask
+from flask import redirect
+from flask import  render_template
+from flask import request
+from flask import  flash
+from flask import  url_for
 import os
 import json
 app = Flask(__name__)
@@ -24,6 +29,7 @@ def index():
 def sign_up():
 	with open('./user/user.json', 'r') as f:
 		userDict = json.load(f)
+
 	if request.method == 'POST':
 		username = request.form.get('username')
 		password = request.form.get('password')
@@ -36,17 +42,55 @@ def sign_up():
 			flash(u"输入不完整")
 		elif password != password2:
 			flash(u"密码不一致")
+		elif len(password) < 6:
+			flash(u'密码太短，不安全，请输入6位以上密码')
 		else:
 			flash(u'注册成功')
 			#保存注册账号
 			with open("./user/user.json", "w") as f:
-				userDict[username] = password
+				d = {}
+				d["password"] = password
+				userDict[username] = d
 				json.dump(userDict, f)
-	return render_template('sign_up.html')
 
+	return render_template('sign_up.html')
 @app.route('/login',methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		with open('./user/user.json', 'r') as f:
+			userDict = f.read()
+			userDict = json.loads(userDict)
+		username = request.form.get('username')
+		password = request.form.get('password')
+
+		#处理用户名不存在的异常
+		if username not in userDict:
+			flash(u'用户名不存在,请注册')
+		elif userDict[username]["password"] == password:
+			flash(u'登陆成功！')
+			return redirect(url_for('person', name=username))
+		else:
+			flash(u'密码错误')
 	return render_template('login.html')
+
+@app.route('/<name>')
+def person(name):
+	with open("./user/user.json", 'r') as f:
+		userDict = json.load(f)
+	friend = userDict.get("friend", 0)
+	print(friend)
+	if friend == 0:
+		my_friend = ["你当前还没有朋友"]
+	else:
+		my_friend = friend
+	return render_template('person.html', name=name, my_friend=my_friend)
+
+@app.route('/chatbot', methods=['GET', 'POST'])
+def chatbot():
+	if request.method == "POST":
+		input_text = request.form.get('input_text')
+		print(input_text)
+	return render_template('chatbot.html')
 
 if __name__ == '__main__':
 	app.run(debug = True)
